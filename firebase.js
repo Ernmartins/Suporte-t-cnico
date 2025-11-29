60b40afeaa0b18f8bb4811b5fb3f8cda4f9342b1
Ernmartins <156158897+Ernmartins@users.noreply.github.com>
2025-11-29 19:24:29 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,12 @@
+<script type="module">
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJe1ZqvaNG0y04PCU3PkUU2XebbI86lt4",
+  authDomain: "ticket-helpdesk-a2dd8.firebaseapp.com",
+  projectId: "ticket-helpdesk-a2dd8",
+  storageBucket: "ticket-helpdesk-a2dd8.firebasestorage.app",
+  messagingSenderId: "464766886414",
+  appId: "1:464766886414:web:cd689936d78102b04b525f",
+  measurementId: "G-9N8M2GXWMD"
+};
+</script>
 
