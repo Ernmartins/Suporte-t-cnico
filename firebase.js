b0dafe108fadd97179e093f856b6666623f901a2
Ernmartins <156158897+Ernmartins@users.noreply.github.com>
2025-11-29 19:51:00 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-<script type="module">
+// firebase.js
 export const firebaseConfig = {
   apiKey: "AIzaSyAJe1ZqvaNG0y04PCU3PkUU2XebbI86lt4",
   authDomain: "ticket-helpdesk-a2dd8.firebaseapp.com",
@@ -8,5 +8,3 @@ export const firebaseConfig = {
   appId: "1:464766886414:web:cd689936d78102b04b525f",
   measurementId: "G-9N8M2GXWMD"
 };
-</script>
-
